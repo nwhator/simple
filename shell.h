62a60b96ded6d55484f36997f7c0ef6f68a64635
ItsJustDice <ckdebruin@gmail.com>
2023-04-26 09:57:01 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define MAX_ARGS 64
+#define BUFFER 1024 /* define constant buffer of 1024 bytes */
+
+/* Library Used */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
